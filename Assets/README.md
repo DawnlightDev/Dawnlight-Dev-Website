35d6873369864816446783bd5b2cc30177aa7870
Dawnlight Dev~ <90069822+DawnlightDev@users.noreply.github.com>
2024-01-17 14:39:39 -0500

    Add files via upload

---
+++ Assets/README.md
@@ -0,0 +1,16 @@
+# Dawnlight Dev's Developer Blog
+
+The repository for my personal blog website, currently hosted on Netlify! This site is programmed using HTML5, CSS3 and JavaScript with the jQuery library for added functionality. This is my personal blog where I will be uploading blog posts relating to projects I'm currently developing and working on, such as my 2D indie JRPG game, <i>Mahou Shoujo Monogatari</i>
+
+## About The Developer
+My name is Dawnlight, also known as Dawnlight Dev or by my old Twitter handle, DawnlightDGArle. I'm a Computer Science and Mathematics student at the University of Maine at Farmington and am in my 4th year of my degree program. I wish to go to grad school to get my PhD in Computer Science so I can become a Professor of Computer Science at a university somewhere in the future, but for now, I'm just a simple developer working on various cool projects and honing my skills in the software engineering world! As for why I created my blog, well it was so that I could practice my technical writing skills in the form of devlogs! I plan on using my site to post various articles about the various projects I'm working on (and perhaps do something in correlation with YouTube later) and to further document my ongoing journey into the world of Computer Science!
+
+<br>
+
+If you happen to like the design of my website or like some of the functionality in my site, feel free to sample from my repository (with credit of course if you use a lot of my code). I would be quite humbled if my blog inspired you to create your own!
+
+## Current Tech Stack
+<ul>
+<li> HTML5 </li>
+<li> CSS3 </li>
+<li> jQuery </li>
---
+++ blogposts.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+  function getPosts() {
+    // fetch all the HTML files in the /blog directory
+    fetch('/blog-posts/')
+      .then(response => response.text())
+      .then(data => {
+        // create an empty array to hold the post data
+        var posts = [];
+
+        // parse the HTML data
+        var parser = new DOMParser();
+        var htmlDoc = parser.parseFromString(data, 'text/html');
+
+        // get all the links to the HTML files
+        var links = htmlDoc.getElementsByTagName('a');
+
+        // loop through the links and fetch the content of each file
+        var promises = [];
+        for (var i = 0; i < links.length; i++) {
+          var link = links[i].getAttribute('href');
+          if (link.endsWith('.html')) {
+            promises.push(
+              (function(link) {
+                return fetch(link)
+                  .then(response => response.text())
+                  .then(postData => {
+                    var parser = new DOMParser();
+                    var postDoc = parser.parseFromString(postData, 'text/html');
+                    var title = postDoc.querySelector('h1#post-title').textContent;
+                    var subtitle = postDoc.querySelector('h2#post-title').textContent;
+                    var content = postDoc.querySelector('p').textContent;
+                    var thumbnailimg = postDoc.querySelector('img#postimg').src;
+                    var postNumber = parseInt(title.match(/\d+/)[0]); // Extract the post number
+                    console.log(title, content);
+
+                    var url = link;
+
+                    // limit the preview to the first 50 words
+                    var preview = content.split(' ').slice(0, 15).join(' ') + '...';
+
+                    // create the HTML markup for each post card
+                    var postMarkup = '<div class="post-card">' + '<div id = "flex-child">' +
+                      '<div id = "cardimg" style="background-image: url(' + thumbnailimg + ');"></div>' +
+                      '<h3>' + title + '</h3>' +
+                      '<h4>' + subtitle + '</h4>' +
+                      '<p>' + preview + '</p>' +
+                      '<a href="' + url + '">Read more</a>' + '</div>' +
+                      '</div>';
+
+                    // add the post data to the posts array
+                    posts.push({ postNumber: postNumber, markup: postMarkup });
+                    console.log(postMarkup);
+                  });
+              })(link)
+            );
+          }
+        }
+
+        // Wait for all promises to resolve, then sort and update the blog container
+        Promise.all(promises).then(() => {
+          posts.sort((a, b) => b.postNumber - a.postNumber); // Sort in descending order
+          var sortedPostsMarkup = posts.map(post => post.markup);
+          $('#devlogs-body').html(sortedPostsMarkup.join(''));
+        });
+      });
+  }
+
+  getPosts();
+});
