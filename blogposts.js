76261a00939febabe013a564eb84b64d2c7005ab
Dawnlight Dev~ <90069822+DawnlightDev@users.noreply.github.com>
2024-01-18 11:33:46 -0500

    Update blogposts.js

--- blogposts.js
+++ blogposts.js
@@ -1,60 +1,45 @@
 $(document).ready(function() {
   function getPosts() {
-    // fetch all the HTML files in the /blog-posts/ directory
-    fetch('/blog-posts/')
-      .then(response => response.text())
+    // fetch all the HTML files in the /blog-posts/ directory from the server
+    fetch('/blog-posts')
+      .then(response => response.json())
       .then(data => {
         // create an empty array to hold the post data
         var posts = [];
 
-        // parse the HTML data
-        var parser = new DOMParser();
-        var htmlDoc = parser.parseFromString(data, 'text/html');
+        // loop through the files and fetch the content of each file
+        var promises = data.files.map(file => {
+          return fetch(`/blog-posts/${file}`)
+            .then(response => response.text())
+            .then(postData => {
+              var parser = new DOMParser();
+              var postDoc = parser.parseFromString(postData, 'text/html');
+              var title = postDoc.querySelector('h1#post-title').textContent;
+              var subtitle = postDoc.querySelector('h2#post-title').textContent;
+              var content = postDoc.querySelector('p').textContent;
+              var thumbnailimg = postDoc.querySelector('img#postimg').src;
+              var postNumber = parseInt(title.match(/\d+/)[0]); // Extract the post number
+              console.log(title, content);
 
-        // get all the links to the HTML files
-        var links = htmlDoc.getElementsByTagName('a');
+              var url = `/blog-posts/${file}`;
 
-        // loop through the links and fetch the content of each file
-        var promises = [];
-        for (var i = 0; i < links.length; i++) {
-          var link = links[i].getAttribute('href');
-          if (link.endsWith('.html')) {
-            promises.push(
-              (function(link) {
-                return fetch(link)
-                  .then(response => response.text())
-                  .then(postData => {
-                    var parser = new DOMParser();
-                    var postDoc = parser.parseFromString(postData, 'text/html');
-                    var title = postDoc.querySelector('h1#post-title').textContent;
-                    var subtitle = postDoc.querySelector('h2#post-title').textContent;
-                    var content = postDoc.querySelector('p').textContent;
-                    var thumbnailimg = postDoc.querySelector('img#postimg').src;
-                    var postNumber = parseInt(title.match(/\d+/)[0]); // Extract the post number
-                    console.log(title, content);
+              // limit the preview to the first 50 words
+              var preview = content.split(' ').slice(0, 15).join(' ') + '...';
 
-                    var url = link;
+              // create the HTML markup for each post card
+              var postMarkup = '<div class="post-card">' + '<div id = "flex-child">' +
+                '<div id = "cardimg" style="background-image: url(' + thumbnailimg + ');"></div>' +
+                '<h3>' + title + '</h3>' +
+                '<h4>' + subtitle + '</h4>' +
+                '<p>' + preview + '</p>' +
+                '<a href="' + url + '">Read more</a>' + '</div>' +
+                '</div>';
 
-                    // limit the preview to the first 50 words
-                    var preview = content.split(' ').slice(0, 15).join(' ') + '...';
-
-                    // create the HTML markup for each post card
-                    var postMarkup = '<div class="post-card">' + '<div id = "flex-child">' +
-                      '<div id = "cardimg" style="background-image: url(' + thumbnailimg + ');"></div>' +
-                      '<h3>' + title + '</h3>' +
-                      '<h4>' + subtitle + '</h4>' +
-                      '<p>' + preview + '</p>' +
-                      '<a href="' + url + '">Read more</a>' + '</div>' +
-                      '</div>';
-
-                    // add the post data to the posts array
-                    posts.push({ postNumber: postNumber, markup: postMarkup });
-                    console.log(postMarkup);
-                  });
-              })(link)
-            );
-          }
-        }
+              // add the post data to the posts array
+              posts.push({ postNumber: postNumber, markup: postMarkup });
+              console.log(postMarkup);
+            });
+        });
 
         // Wait for all promises to resolve, then sort and update the blog container
         Promise.all(promises).then(() => {
