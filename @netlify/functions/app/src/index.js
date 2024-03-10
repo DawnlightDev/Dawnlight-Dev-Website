0a2ef7ce89e848d58eb59f95d453845d8099c6f4
Dawnlight Dev~ <90069822+DawnlightDev@users.noreply.github.com>
2024-03-09 20:37:29 -0500

    Delete @netlify directory

--- @netlify/functions/app/src/index.js
+++
@@ -1,9 +0,0 @@
-// functions/app/src/index.js
-
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
-
-// Your existing server code goes here
-
-module.exports.handler = serverless(app);
