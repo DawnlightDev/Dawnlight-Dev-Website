9d3a2bd03b9343fed9b4ac86a4f2e601fab9dcf3
Dawnlight Dev~ <90069822+DawnlightDev@users.noreply.github.com>
2024-03-09 20:38:45 -0500

    Delete handler.js

--- handler.js
+++
@@ -1,9 +0,0 @@
-// handler.js
-
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
-
-// Your existing server code goes here
-
-module.exports.handler = serverless(app);
