84779be1ca66c7c5c9fbb1ce94e1e0054695133d
Dawnlight Dev~ <90069822+DawnlightDev@users.noreply.github.com>
2024-03-11 17:32:41 -0400

    Update blogposts.js

--- blogposts.js
+++ blogposts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     function getPosts() {
     // fetch all the HTML files in the /blog directory
-      fetch('https://taupe-kitsune-d0a191.netlify.app/blog-posts/')
+      fetch('https://dawnlight.dev/blog-posts/')
       .then(response => response.text())
       .then(data => {
         console.log("Successfully found directory!");
